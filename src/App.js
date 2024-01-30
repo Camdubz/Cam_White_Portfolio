b364b38efb10d2b44589cf96e4853740525ecf0e
Cameron <camwhite982@gmail.com>
2024-01-31 10:12:10 +1300

    homepage-route

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/home" element={<Homepage />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/a2xcase" element={<A2XCase />} />
         <Route path="/about" element={<About />} />
