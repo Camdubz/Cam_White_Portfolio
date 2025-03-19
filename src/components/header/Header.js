fb4e13b8acfe25bb09b13ba87ed77acaa698a6b0
Camdubz <camwhite982@gmail.com>
2025-03-19 15:38:26 +1300

    responsiveness and general fixes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,11 +4,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="hero-container">
-      <img
-        className="home-banner-graphic"
-        src="./images/meat-pie.png"
-        alt="banner home svg"
-      />
+     
       <div className="hero-text">
         <h1>Your SEO is like a soggy pie: Technically edible, but no one’s bragging.</h1>
         <p>We’ll bake up a strategy so crisp, even Google asks for seconds. </p>
@@ -16,6 +12,11 @@ const Header = () => {
           <button className="btn btn-light">Serve Me Crispy SEO </button>
         </a>
       </div>
+       <img
+        className="home-banner-graphic"
+        src="./images/meat-pie.png"
+        alt="banner home svg"
+      />
     </div>
   );
 };
